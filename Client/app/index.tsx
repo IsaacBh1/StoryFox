36d0017c65203e7eee4a173aa6931b1b5c1534c2
Mohamed Islam <mohammedislam.benyahkem@gmail.com>
2025-03-18 15:01:01 +0100

    another

--- Client/app/index.tsx
+++ Client/app/index.tsx
@@ -3,6 +3,7 @@ import { Link } from "expo-router";
 import { View } from "react-native";
 import { Button } from "tamagui";
 import * as NavigationBar from "expo-navigation-bar";
+import { useEffect } from "react";
 
 // import "@/global.css";
 export default function Index() {
@@ -23,10 +24,11 @@ export default function Index() {
           Hello World
         </Link>
       </Button>
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Link href="./story_parameters/options" asChild>
-        <Button theme="light_green">select gender</Button>
-      </Link>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Link href="./story_parameters/options" asChild>
+          <Button theme="light_green">select gender</Button>
+        </Link>
+      </View>
     </View>
   );
 }
